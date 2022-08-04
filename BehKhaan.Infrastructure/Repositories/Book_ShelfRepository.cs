d97c0842e6ac7e4b1adcb3721d26d2cb60988e24
Alireza <arnn379@gmail.com>
2022-08-04 13:20:22 +0430

    All Repositories Added

--- BehKhaan.Infrastructure/Repositories/Book_ShelfRepository.cs
+++ BehKhaan.Infrastructure/Repositories/Book_ShelfRepository.cs
@@ -1,5 +1,7 @@
 ﻿using BehKhaan.Domain.Entities;
 using BehKhaan.Domain.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +12,35 @@ namespace BehKhaan.Infrastructure.Repositories
 {
     public class Book_ShelfRepository : IBook_ShelfRepository
     {
+        private readonly AppDbContext _context;
+        public Book_ShelfRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public void Edit(Book_Shelf entity)
         {
-            throw new NotImplementedException();
+            EntityEntry entityEntry = _context.Entry<Book_Shelf>(entity);
+            entityEntry.State = EntityState.Modified;
+            _context.SaveChanges();
         }
 
-        public IEnumerable<Book_Shelf> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Book_Shelf> GetAll() => _context.Books_Shelfs.ToList();
 
-        public Book_Shelf GetById(string bookId, string shelfId)
-        {
-            throw new NotImplementedException();
-        }
+        public Book_Shelf GetById(string bookId, string shelfId) => _context.Books_Shelfs.FirstOrDefault(bs => bs.Id == id);
 
         public void Insert(Book_Shelf entity)
         {
-            throw new NotImplementedException();
+            _context.Books_Shelfs.Add(entity);
+            _context.SaveChanges();
         }
 
         public void Remove(string bookId, string shelfId)
         {
-            throw new NotImplementedException();
+            var entity = _context.Books_Shelfs.FirstOrDefault(bs => bs.Id == id);
+            EntityEntry entityEntry = _context.Entry<Book_Shelf>(entity);
+            entityEntry.State = EntityState.Deleted;
+            _context.SaveChanges();
         }
     }
 }
